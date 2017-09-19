17c1323349dd6aecf316a7184240df823440a3c1
Ho Jie Feng <hjf3108@gmail.com>
2017-09-19 23:00:24 +0800

    add download as zip

--- routes/images.js
+++ routes/images.js
@@ -11,6 +11,8 @@ var gm = require('gm').subClass({imageMagick: true});
 Promise.promisifyAll(gm.prototype);
 var _ = require('lodash');
 var request = require('request');
+var archiver = require('archiver');
+var sanitize = require("sanitize-filename");
 
 /* GET images listing. */
 router.get('/', function(req, res, next) {
@@ -80,6 +82,39 @@ router.get('/', function(req, res, next) {
 });
 
 
+
+router.get('/download', function(req, res, next) {
+
+  var query = req.query.query;
+
+  if(query)
+    res.header('Content-Disposition', "attachment; filename=images_" + sanitize(query) + ".zip")
+  else
+    res.header('Content-Disposition', "attachment; filename=images_all.zip")
+  res.header('Content-Type','application/zip')
+
+  images.getRange(0, 10000000 ,query)
+  .then(function(images){
+    var archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+    });
+    archive.pipe(res);
+    for(i in images)
+    {
+      var image = images[i];
+      var file = 'public/uploads/' + image.hash + "." + mime.extension(image.mimetype)
+      var filename = image.hash + "_" +
+        image.width + "x" + image.height + "px " +
+        image.character + "_" +
+        image.emotion + "_" +
+        image.text + "_" +
+        image.tags + "_" +
+        image.comments + "_";
+        archive.append(fs.createReadStream(file), { name: sanitize(filename.replace(/ /g, "_")) + "." + mime.extension(image.mimetype) });
+    }
+    archive.finalize();
+  })
+})
 
 router.get('/upload', function(req, res, next) {
   res.render('imageupload', { title: 'Image Upload' });
